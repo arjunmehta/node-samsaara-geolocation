582130039760479e782c807955c15e30461fc35f
Arjun Mehta <arjunmeht@gmail.com>
2014-06-05 12:10:12 -0400

    Removing all unecessary closures in modules

--- main.js
+++ main.js
@@ -6,88 +6,86 @@
 
 var debug = require('debug')('samsaara:geoPosition');
 
-function geoPosition(options){
 
-  var samsaara,
-      config,
-      connectionController,
-      communication,
-      ipc;
+var samsaara,
+    config,
+    connectionController,
+    communication,
+    ipc;
 
 
-  /**
-   * Connection Initialization Methods
-   * Called for every new connection
-   *
-   * @opts: {Object} contains the connection's options
-   * @connection: {SamsaaraConnection} the connection that is initializing
-   * @attributes: {Attributes} The attributes of the SamsaaraConnection and its methods
-   */
+var geoPosition = {
 
-  function connectionInitialzation(opts, connection, attributes){
+  name: "geoPosition",
 
-    connection.updateDataAttribute("geoposition", null);
+  clientScript: __dirname + '/client/samsaara-geoposition.js', 
 
-    if(opts.geoPosition !== undefined){
-      debug("Initializing geoPosition...");
-      if(opts.geoPosition === "force") attributes.force("geoPosition");
-      connection.executeRaw({ns:"internal", func:"getGeoPosition"}, geoPositionReturn);
-    }    
+  connectionInitialization: {
+    geoPosition: connectionInitialzation
   }
+};
 
 
-  /**
-   * Foundation Methods
-   */
+// the root interface loaded by require. Options are pass in options here.
 
-  function geoPositionReturn(err, geoposition){
-
-    var connection = this;
-
-    if(connection !== undefined){
-      connection.updateDataAttribute("geoposition", geoposition);
-      connection.initializeAttributes.initialized(err, "geoPosition");
-      samsaara.emit('geoPosition', connection, err, geoposition);
-      debug("geoPosition Retrieval Success", err, geoposition);
-    }
-    else{
-      connection.initializeAttributes.initialized(new Error("Unknown Error: GeoPosition did not work"), "geoPosition");
-      debug("geoPosition Retrieval Error", connection);
-    }
-  }
-
-  /**
-   * Module Return Function.
-   * Within this function you should set up and return your samsaara middleWare exported
-   * object. Your eported object can contain:
-   * name, foundation, remoteMethods, connectionInitialization, connectionClose
-   */
-
-  return function geoPosition(samsaaraCore){
-
-    samsaara = samsaaraCore.samsaara;
-    connectionController = samsaaraCore.connectionController;
-    communication = samsaaraCore.communication;
-    ipc = samsaaraCore.ipc;
-
-    samsaaraCore.addClientFileRoute("samsaara-geoposition.js", __dirname + '/client/samsaara-geoposition.js');
-
-    var exported = {
-
-      name: "geoPosition",
-
-      clientScript: __dirname + '/client/samsaara-geoposition.js', 
-
-      connectionInitialization: {
-        geoPosition: connectionInitialzation
-      }
-
-    };
-
-    return exported;
-
-  };
-
+function main(opts){
+  return initialize;
 }
 
-module.exports = exports = geoPosition;
+
+// samsaara will call this method when it's ready to load it into its middleware stack
+// return your main
+
+function initialize(samsaaraCore){
+
+  samsaara = samsaaraCore.samsaara;
+  connectionController = samsaaraCore.connectionController;
+  communication = samsaaraCore.communication;
+  ipc = samsaaraCore.ipc;
+
+  samsaaraCore.addClientFileRoute("samsaara-geoposition.js", __dirname + '/client/samsaara-geoposition.js');
+
+  return geoPosition;
+}
+
+
+// Connection Initialization Methods
+// Called for every new connection
+// 
+// @opts: {Object} contains the connection's options
+// @connection: {SamsaaraConnection} the connection that is initializing
+// @attributes: {Attributes} The attributes of the SamsaaraConnection and its methods
+
+function connectionInitialzation(opts, connection, attributes){
+
+  connection.updateDataAttribute("geoposition", null);
+
+  if(opts.geoPosition !== undefined){
+    debug("Initializing geoPosition...");
+    if(opts.geoPosition === "force") attributes.force("geoPosition");
+    connection.executeRaw({ns:"internal", func:"getGeoPosition"}, geoPositionReturn);
+  }    
+}
+
+
+// Foundation Methods
+
+function geoPositionReturn(err, geoposition){
+
+  var connection = this;
+
+  if(connection !== undefined){
+    connection.updateDataAttribute("geoposition", geoposition);
+    connection.initializeAttributes.initialized(err, "geoPosition");
+    samsaara.emit('geoPosition', connection, err, geoposition);
+    debug("geoPosition Retrieval Success", err, geoposition);
+  }
+  else{
+    connection.initializeAttributes.initialized(new Error("Unknown Error: GeoPosition did not work"), "geoPosition");
+    debug("geoPosition Retrieval Error", connection);
+  }
+}
+
+
+
+module.exports = exports = main;
